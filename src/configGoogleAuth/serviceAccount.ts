e971cd882b3c5df057217117f8bcee461e17b7cc
franjavi8776 <franjavi871976@gmail.com>
2024-12-17 23:23:54 -0400

    fix error

--- src/configGoogleAuth/serviceAccount.ts
+++ src/configGoogleAuth/serviceAccount.ts
@@ -31,17 +31,17 @@ const {
 // }
 
 const serviceAccount = {
-  type: FIREBASE_TYPE,
-  project_id: FIREBASE_PROJECT_ID,
-  private_key_id: FIREBASE_PRIVATE_KEY_ID,
-  private_key: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  client_email: FIREBASE_CLIENT_EMAIL,
-  client_id: FIREBASE_CLIENT_ID,
-  auth_uri: FIREBASE_AUTH_URI,
-  token_uri: FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER,
-  client_x509_cert_url: FIREBASE_CLIENT_URL,
-  universe_domain: FIREBASE_UNIVERSE_DOMAIN,
+  type: FIREBASE_TYPE || "",
+  project_id: FIREBASE_PROJECT_ID || "",
+  private_key_id: FIREBASE_PRIVATE_KEY_ID || "",
+  private_key: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
+  client_email: FIREBASE_CLIENT_EMAIL || "",
+  client_id: FIREBASE_CLIENT_ID || "",
+  auth_uri: FIREBASE_AUTH_URI || "",
+  token_uri: FIREBASE_TOKEN_URI || "",
+  auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER || "",
+  client_x509_cert_url: FIREBASE_CLIENT_URL || "",
+  universe_domain: FIREBASE_UNIVERSE_DOMAIN || "",
 };
 
 export default serviceAccount;
