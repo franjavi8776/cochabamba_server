1c5e01bae9c08f61bc1e026dfaa492ade8eae480
franjavi8776 <franjavi871976@gmail.com>
2024-12-22 12:47:41 -0400

    change db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,20 +2,11 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 
-if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DATABASE_URL) {
-  throw new Error(
-    "Environment variables DB_NAME, DB_USER, and DB_PASSWORD must be defined"
-  );
+if (!DATABASE_URL) {
+  throw new Error("Environment variable DATABASE_URL must be defined");
 }
-
-//! DEV
-// export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD,  {
-//   host: "localhost",
-//   dialect: "postgres",
-//   logging: false,
-// });
 
 //! PRODUCTION
 export const sequelize = new Sequelize(DATABASE_URL, {
