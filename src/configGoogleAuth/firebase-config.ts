ec10b88e305a95ecb5ca187449e1b0e63361883d
franjavi8776 <franjavi871976@gmail.com>
2024-12-22 11:16:49 -0400

    fix error with try/catch

--- src/configGoogleAuth/firebase-config.ts
+++ src/configGoogleAuth/firebase-config.ts
@@ -1,8 +1,12 @@
 import * as admin from "firebase-admin";
 import * as serviceAccount from "./serviceAccount.json";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+} catch (error) {
+  console.error("Firebase admin initialization error", error);
+}
 
 export default admin;
