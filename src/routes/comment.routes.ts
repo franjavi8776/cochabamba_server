aa9cd42c02a59e380fc29e9f5ee0ea9cc337ea90
franjavi8776 <franjavi871976@gmail.com>
2025-01-06 11:23:32 -0400

    add routes at comment routes

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -3,16 +3,28 @@ import {
   createComment,
   deleteComment,
   getAllComments,
+  getCommentsByEmergency,
+  getCommentsByGym,
+  getCommentsByHotel,
+  getCommentsByMovieTheater,
   getCommentsByRestaurant,
+  getCommentsBySupermarket,
   getCommentsByTaxi,
+  getCommentsByTourism,
 } from "../controllers/comment.controllers";
 
 const router = Router();
 
+router.get("/comments", getAllComments);
 router.get("/comments/restaurant/:id", getCommentsByRestaurant);
 router.get("/comments/taxi/:id", getCommentsByTaxi);
+router.get("/comments/hotel/:id", getCommentsByHotel);
+router.get("/comments/emergency/:id", getCommentsByEmergency);
+router.get("/comments/gym/:id", getCommentsByGym);
+router.get("/comments/movieTheater/:id", getCommentsByMovieTheater);
+router.get("/comments/supermarket/:id", getCommentsBySupermarket);
+router.get("/comments/tourism/:id", getCommentsByTourism);
 router.post("/comments", createComment);
-router.get("/comments", getAllComments);
 router.delete("/comments/:id", deleteComment);
 
 export default router;
